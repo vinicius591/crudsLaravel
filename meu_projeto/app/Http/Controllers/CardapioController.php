b4411508d043ec896cfde0aa9bc1b37eb2b8d289
unknown <alicelimadocarmo@outlook.com>
2026-06-16 11:52:34 -0300

    criação dos aqvs

---
+++ meu_projeto/app/Http/Controllers/CardapioController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cardapio;
+use Illuminate\Http\Request;
+
+class CardapioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cardapio $cardapio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Cardapio $cardapio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Cardapio $cardapio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cardapio $cardapio)
+    {
+        //
+    }
+}
